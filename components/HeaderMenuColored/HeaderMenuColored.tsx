790797a54be7759ef98cb7334a8c4b688ef6e516
Ymir <36711026+Ymirke@users.noreply.github.com>
2022-04-01 11:35:56 +0300

    Making link links optional in HeaderMenuColored

--- components/HeaderMenuColored/HeaderMenuColored.tsx
+++ components/HeaderMenuColored/HeaderMenuColored.tsx
@@ -50,7 +50,7 @@ const useStyles = createStyles((theme) => ({
 }));
 
 interface HeaderSearchProps {
-  links: { link: string; label: string; links: { link: string; label: string }[] }[];
+  links: { link: string; label: string; links?: { link: string; label: string }[] }[];
 }
 
 export function HeaderMenuColored({ links }: HeaderSearchProps) {
