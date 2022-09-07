1168a52933f49cc0dc4e135dc5f2438cf8de1a0d
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-09-07 19:21:01 +0300

    Update NotFoundImage.tsx

--- components/NotFoundImage/NotFoundImage.tsx
+++ components/NotFoundImage/NotFoundImage.tsx
@@ -1,7 +1,17 @@
-import { createStyles, Image, Container, Title, Text, Button, SimpleGrid } from '@mantine/core';
-import image from './image.svg';
+import Link from "next/link";
 
-const useStyles = createStyles((theme) => ({
+import {
+  Button,
+  Container,
+  createStyles,
+  Image,
+  SimpleGrid,
+  Text,
+  Title,
+} from "@mantine/core";
+import image from "./images/404.svg";
+
+const useStyles = createStyles(theme => ({
   root: {
     paddingTop: 80,
     paddingBottom: 80,
@@ -13,48 +23,54 @@ const useStyles = createStyles((theme) => ({
     marginBottom: theme.spacing.md,
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
 
-    [theme.fn.smallerThan('sm')]: {
+    [theme.fn.smallerThan("sm")]: {
       fontSize: 32,
     },
   },
 
   control: {
-    [theme.fn.smallerThan('sm')]: {
-      width: '100%',
+    [theme.fn.smallerThan("sm")]: {
+      width: "100%",
     },
   },
 
-  mobileImage: {
-    [theme.fn.largerThan('sm')]: {
-      display: 'none',
-    },
-  },
-
-  desktopImage: {
-    [theme.fn.smallerThan('sm')]: {
-      display: 'none',
+  container404: {
+    display: "flex",
+    flexDirection: "row-reverse",
+    [theme.fn.smallerThan("sm")]: {
+      flexDirection: "column",
     },
   },
 }));
 
-export function NotFoundImage() {
+export default function NotFoundImage() {
   const { classes } = useStyles();
 
   return (
     <Container className={classes.root}>
-      <SimpleGrid spacing={80} cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1, spacing: 40 }]}>
-        <Image src={image.src} className={classes.mobileImage} />
+      <SimpleGrid
+        spacing={80}
+        cols={2}
+        breakpoints={[{ maxWidth: "sm", cols: 1, spacing: 40 }]}
+        className={classes.container404}>
+        <Image src={image.src} />
         <div>
           <Title className={classes.title}>Something is not right...</Title>
           <Text color="dimmed" size="lg">
-            Page you are trying to open does not exist. You may have mistyped the address, or the
-            page has been moved to another URL. If you think this is an error contact support.
+            Page you are trying to open does not exist. You may have mistyped
+            the address, or the page has been moved to another URL. If you think
+            this is an error contact support.
           </Text>
-          <Button variant="outline" size="md" mt="xl" className={classes.control}>
-            Get back to home page
-          </Button>
+          <Link href="/">
+            <Button
+              variant="outline"
+              size="md"
+              mt="xl"
+              className={classes.control}>
+              Get back to home page
+            </Button>
+          </Link>
         </div>
-        <Image src={image.src} className={classes.desktopImage} />
       </SimpleGrid>
     </Container>
   );
