142fe985f73e481d7320b1f1f6c7e865c3bce66e
Vitaly Rtishchev <rtivital@gmail.com>
2022-09-17 18:46:38 +0400

    Add CarouselCard component

--- components/CarouselCard/CarouselCard.tsx
+++ components/CarouselCard/CarouselCard.tsx
@@ -1,9 +1,101 @@
+import { createStyles, Image, Card, Text, Group, Button } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
+import { IconStar } from '@tabler/icons';
 
-import { createStyles } from '@mantine/core';
+const useStyles = createStyles((theme, _params, getRef) => ({
+  price: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  },
 
-const useStyles = createStyles((theme) => ({}));
+  carousel: {
+    '&:hover': {
+      [`& .${getRef('carouselControls')}`]: {
+        opacity: 1,
+      },
+    },
+  },
+
+  carouselControls: {
+    ref: getRef('carouselControls'),
+    transition: 'opacity 150ms ease',
+    opacity: 0,
+  },
+
+  carouselIndicator: {
+    width: 4,
+    height: 4,
+    transition: 'width 250ms ease',
+
+    '&[data-active]': {
+      width: 16,
+    },
+  },
+}));
+
+const images = [
+  'https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+  'https://images.unsplash.com/photo-1567767292278-a4f21aa2d36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+  'https://images.unsplash.com/photo-1605774337664-7a846e9cdf17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+  'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+  'https://images.unsplash.com/photo-1616486029423-aaa4789e8c9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+];
 
 export function CarouselCard() {
   const { classes } = useStyles();
-  return <div>CarouselCard</div>;
+
+  const slides = images.map((image) => (
+    <Carousel.Slide key={image}>
+      <Image src={image} height={220} />
+    </Carousel.Slide>
+  ));
+
+  return (
+    <Card radius="md" withBorder p="xl">
+      <Card.Section>
+        <Carousel
+          withIndicators
+          loop
+          classNames={{
+            root: classes.carousel,
+            controls: classes.carouselControls,
+            indicator: classes.carouselIndicator,
+          }}
+        >
+          {slides}
+        </Carousel>
+      </Card.Section>
+
+      <Group position="apart" mt="lg">
+        <Text weight={500} size="lg">
+          Forde, Norway
+        </Text>
+
+        <Group spacing={5}>
+          <IconStar size={16} />
+          <Text size="xs" weight={500}>
+            4.78
+          </Text>
+        </Group>
+      </Group>
+
+      <Text size="sm" color="dimmed" mt="sm">
+        Relax, rejuvenate and unplug in this unique contemporary Birdbox. Feel close to nature in
+        ultimate comfort. Enjoy the view of the epic mountain range of Blegja and the Førdefjord.
+      </Text>
+
+      <Group position="apart" mt="md">
+        <div>
+          <Text size="xl" span weight={500} className={classes.price}>
+            397$
+          </Text>
+          <Text span size="sm" color="dimmed">
+            {' '}
+            / night
+          </Text>
+        </div>
+
+        <Button radius="md">Book now</Button>
+      </Group>
+    </Card>
+  );
 }
