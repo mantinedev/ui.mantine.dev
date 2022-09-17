7adff7b90a4f6010f6b95f59f358cded8cf6e1fb
Vitaly Rtishchev <rtivital@gmail.com>
2022-09-17 22:22:59 +0400

    Cleanup SwitchToggle to use new Switch features

--- components/SwitchToggle/SwitchToggle.tsx
+++ components/SwitchToggle/SwitchToggle.tsx
@@ -1,43 +1,19 @@
-import { createStyles, Switch, Group, useMantineColorScheme } from '@mantine/core';
+import { Switch, Group, useMantineColorScheme, useMantineTheme } from '@mantine/core';
 import { IconSun, IconMoonStars } from '@tabler/icons';
-
-const useStyles = createStyles((theme) => ({
-  root: {
-    position: 'relative',
-    '& *': {
-      cursor: 'pointer',
-    },
-  },
-
-  icon: {
-    pointerEvents: 'none',
-    position: 'absolute',
-    zIndex: 1,
-    top: 3,
-  },
-
-  iconLight: {
-    left: 4,
-    color: theme.white,
-  },
-
-  iconDark: {
-    right: 4,
-    color: theme.colors.gray[6],
-  },
-}));
 
 export function SwitchToggle() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  const { classes, cx } = useStyles();
+  const theme = useMantineTheme();
 
   return (
     <Group position="center" my={30}>
-      <div className={classes.root}>
-        <IconSun className={cx(classes.icon, classes.iconLight)} size={18} stroke={1.5} />
-        <IconMoonStars className={cx(classes.icon, classes.iconDark)} size={18} stroke={1.5} />
-        <Switch checked={colorScheme === 'dark'} onChange={() => toggleColorScheme()} size="md" />
-      </div>
+      <Switch
+        checked={colorScheme === 'dark'}
+        onChange={() => toggleColorScheme()}
+        size="lg"
+        onLabel={<IconSun color={theme.white} size={20} stroke={1.5} />}
+        offLabel={<IconMoonStars color={theme.colors.gray[6]} size={20} stroke={1.5} />}
+      />
     </Group>
   );
 }
