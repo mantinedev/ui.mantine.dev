66b13b4bc98265adcc2f342439cc1d9e7e956e89
Vitaly Rtishchev <rtivital@gmail.com>
2022-09-17 21:44:58 +0400

    Add title to changelog page

--- src/components/ChangelogPage/ChangelogPage.tsx
+++ src/components/ChangelogPage/ChangelogPage.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { Container, Title } from '@mantine/core';
 import { upperFirst } from '@mantine/hooks';
@@ -10,13 +11,18 @@ interface ChangelogPageProps {
 
 export function ChangelogPage({ components }: ChangelogPageProps) {
   const router = useRouter();
-  const changelogDate = (router.query.changelog as string).replace('-', ' ');
+  const title = `New components: ${upperFirst(
+    (router.query.changelog as string).replace('-', ' ')
+  )}`;
   const canvases = components.map((component, index) => (
     <ComponentCanvas {...component} key={component.slug} zIndex={components.length - index} />
   ));
 
   return (
-    <div>
+    <>
+      <Head>
+        <title>{`${title} | Mantine UI`}</title>
+      </Head>
       <Container size="xl" mt={50}>
         <Title
           sx={(theme) => ({
@@ -26,10 +32,10 @@ export function ChangelogPage({ components }: ChangelogPageProps) {
           weight={900}
           mb="xl"
         >
-          New components: {upperFirst(changelogDate)}
+          {title}
         </Title>
         {canvases}
       </Container>
-    </div>
+    </>
   );
 }
