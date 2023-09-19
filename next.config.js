1501be1ca9c75a5e0425639a4d1d0fe5bff0f101
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-19 22:41:53 +0400

    Add trailing slashes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: false,
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
