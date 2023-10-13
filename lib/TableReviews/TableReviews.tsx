5aed307bca3b42f69a4e27d7759b2c2dc08830b2
Matheus Peviani Vellone <matheusvellone@gmail.com>
2023-10-13 19:27:15 -0300

    Fix typo

--- lib/TableReviews/TableReviews.tsx
+++ lib/TableReviews/TableReviews.tsx
@@ -94,7 +94,7 @@ export function TableReviews() {
           <Table.Tr>
             <Table.Th>Book title</Table.Th>
             <Table.Th>Year</Table.Th>
-            <Table.Th>AuTable.Thor</Table.Th>
+            <Table.Th>Author</Table.Th>
             <Table.Th>Reviews</Table.Th>
             <Table.Th>Reviews distribution</Table.Th>
           </Table.Tr>
