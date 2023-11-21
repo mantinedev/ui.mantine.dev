2d4885622e5f71aee94504c4aa21e213340f4cc3
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-21 10:36:52 +0400

    Update dependencies to the latest version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../lib/**/*.(stories|story).@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', 'storybook-dark-mode', '@storybook/addon-styling'],
+  addons: ['storybook-dark-mode', '@storybook/addon-styling-webpack'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
