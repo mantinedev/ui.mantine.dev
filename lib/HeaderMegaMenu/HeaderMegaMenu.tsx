61383295050a6d2fb4aa32e557f1d1f0d1b25d50
KaziQ <murat_kadir_aksoy@hotmail.com>
2024-12-03 20:27:35 +0300

    Update HeaderMegaMenu.tsx
    Interferes with AppShell.Main as described in 
    https://github.com/orgs/mantinedev/discussions/4931#discussioncomment-11448770

--- lib/HeaderMegaMenu/HeaderMegaMenu.tsx
+++ lib/HeaderMegaMenu/HeaderMegaMenu.tsx
@@ -86,7 +86,7 @@ export function HeaderMegaMenu() {
   ));
 
   return (
-    <Box pb={120}>
+    <Box>
       <header className={classes.header}>
         <Group justify="space-between" h="100%">
           <MantineLogo size={30} />
