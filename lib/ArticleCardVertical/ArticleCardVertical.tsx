6e7f4604f453d82715ff0a9b3b6e575264ef1cf7
Vitaly Rtishchev <rtivital@gmail.com>
2025-11-01 17:38:08 +0300

    Improve ArticleCardVertical

--- lib/ArticleCardVertical/ArticleCardVertical.tsx
+++ lib/ArticleCardVertical/ArticleCardVertical.tsx
@@ -1,34 +1,52 @@
 import { Avatar, Card, Group, Image, Text } from '@mantine/core';
 import classes from './ArticleCardVertical.module.css';
 
+interface ArticleCardVerticalData {
+  category: string;
+  title: string;
+  authorName: string;
+  authorAvatar: string;
+  date: string;
+  image: string;
+}
+
+const data: ArticleCardVerticalData = {
+  category: 'technology',
+  title: 'The best laptop for Frontend engineers in 2022',
+  authorName: 'Elsa Typechecker',
+  authorAvatar:
+    'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png',
+  date: 'Feb 6th',
+  image:
+    'https://images.unsplash.com/photo-1602080858428-57174f9431cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+};
+
 export function ArticleCardVertical() {
   return (
     <Card withBorder radius="md" p={0} className={classes.card}>
-      <Image
-        src="https://images.unsplash.com/photo-1602080858428-57174f9431cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80"
-        className={classes.image}
-      />
+      <Image src={data.image} className={classes.image} alt={data.title} />
 
       <div className={classes.body}>
-        <Text tt="uppercase" c="dimmed" fw={700} size="xs">
-          technology
+        <Text tt="uppercase" opacity={0.8} fw={700} size="xs">
+          {data.category}
         </Text>
         <Text className={classes.title} mt="xs" mb="md">
-          The best laptop for Frontend engineers in 2022
+          {data.title}
         </Text>
-        <Group wrap="nowrap" gap="xs">
-          <Group gap="xs" wrap="nowrap">
-            <Avatar
-              size={20}
-              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
-            />
-            <Text size="xs">Elsa Typechecker</Text>
+        <Group gap="xs">
+          <Group gap={7}>
+            <Avatar size={20} src={data.authorAvatar} alt={data.authorName} />
+            <Text size="xs" c="bright">
+              {data.authorName}
+            </Text>
           </Group>
-          <Text size="xs" c="dimmed">
+
+          <Text span size="xs" opacity={0.8}>
             •
           </Text>
-          <Text size="xs" c="dimmed">
-            Feb 6th
+
+          <Text size="xs" opacity={0.8}>
+            {data.date}
           </Text>
         </Group>
       </div>
